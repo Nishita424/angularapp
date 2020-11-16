70fb595ef657bc1893fdfa5ace81de051ec860c1
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-16 15:08:25 +0530

    Created a new post component to get the active post with given post id in the url

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { PostService } from '../../services/post.service';
+
+import { Post } from '../../models/post';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css'],
+})
+export class PostComponent implements OnInit {
+  post: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private Location: Location
+  ) {}
+
+  ngOnInit(): void {
+    // We actually get the current active route from ActivatedRoute
+    // To get a param: id(here) from the current active route
+    const id = +this.route.snapshot.paramMap.get('id');
+    // console.log(id);
+    this.postService.getPost(id).subscribe((post) => (this.post = post));
+  }
+}
